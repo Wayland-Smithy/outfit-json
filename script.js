2a42a77e4c2d9d106d3d6b7996dc35501c04e64f
Wayland-Smithy <64715958+Wayland-Smithy@users.noreply.github.com>
2021-10-01 19:13:37 -0700

    Add list age to footer

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ function regenJSON() {
   $('#code-out').val(JSON.stringify(drip, null, 2));
 }
 
+function apiRequest(type, path, params, callback) {
+  let url = `https://api.github.com/${type}/${path}?${params}`;
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', url, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
 $(function () {
   // highlight all text on click for easy copy
   $('#code-out').on('click', function () {
@@ -29,7 +42,7 @@ $(function () {
   // outfit_options is populated by scripts included in the body before this one
   for (const slot of slot_list) {
     for (const option in outfit_options[slot]) {
-      if(outfit_options[slot][option].indexOf('[]') !== -1)
+      if (outfit_options[slot][option].indexOf('[]') !== -1)
         continue;
       $(`#${slot}`).append(`<option value="${option}">${outfit_options[slot][option]}</option>`);
     }
@@ -41,4 +54,11 @@ $(function () {
   // init the form selects and output
   $('.chosen-select').chosen({ allow_single_deselect: true });
   regenJSON();
+  apiRequest('repos',
+    'Wayland-Smithy/outfit-json/commits',
+    'path=/outfit_options&per_page=1',
+    (res) => {
+      let data = JSON.parse(res);
+      $('#update-time').text(dateFns.distanceInWordsToNow(data[0].commit.committer.date));
+    });
 });
